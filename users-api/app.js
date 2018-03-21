3a07456cb14d6a1463cae36fdf64989b14251178
kelley <kell3991@fredonia.edu>
2018-03-21 19:56:07 -0400

    updated to match upstream master, resolve conflicts

--- users-api/app.js
+++ users-api/app.js
@@ -1,9 +1,7 @@
 // app.js
-<<<<<<< HEAD
-=======
 
 //pull in dependencies
->>>>>>> 92743f067c54100a4a21343050755eb224321eb5
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -13,12 +11,6 @@ const app = express();
 //populate req.body with whatever users send
 app.use(bodyParser.json());
 
-<<<<<<< HEAD
-// TODO Add require routers here
-const usersRoutes = require('./routes/users.js');
-app.use('/routes/users', usersRoutes);
-// app.use('/somepath', require(./routes/somepath.js));
-=======
 // Add require routers
 const usersRoutes = require('./routes/users.js');
 app.use('/routes/users', usersRoutes);
@@ -26,7 +18,6 @@ app.use('/routes/users', usersRoutes);
 const loginRoutes = require('./routes/login.js');
 app.use('/login', loginRoutes);
 
->>>>>>> 92743f067c54100a4a21343050755eb224321eb5
 app.use('/version', require('./routes/version.js'));
 app.use('/signup', require('./routes/signup.js'));
 
